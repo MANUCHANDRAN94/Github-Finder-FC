35c00bef4dd35e279029da35cce2a3dba15437a4
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-10-13 00:04:52 +0530

    Updates

---
+++ precache-manifest.2b097795bd84c3f80401c7634fc0198f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcdf4543ac0211c818648373afe6a05",
+    "url": "/Github-Finder-FC/index.html"
+  },
+  {
+    "revision": "e5037099d88d6b8efcc7",
+    "url": "/Github-Finder-FC/static/css/main.c1ef5e3b.chunk.css"
+  },
+  {
+    "revision": "9aa85adf0b4e17c226e0",
+    "url": "/Github-Finder-FC/static/js/2.57fde6a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-Finder-FC/static/js/2.57fde6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5037099d88d6b8efcc7",
+    "url": "/Github-Finder-FC/static/js/main.c9c8fc6d.chunk.js"
+  },
+  {
+    "revision": "06f7b87aa7fdaafb4503",
+    "url": "/Github-Finder-FC/static/js/runtime-main.3b281595.js"
+  },
+  {
+    "revision": "37533f845999566c92b4fc1183b1d5bb",
+    "url": "/Github-Finder-FC/static/media/spinner.37533f84.gif"
+  }
+]);
